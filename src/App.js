59e4be3c6e2508ed38d5b384ea06509b735a0ca9
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-27 21:25:03 +0600

    Added homework

--- src/App.js
+++ src/App.js
@@ -1,52 +1,57 @@
-import ArticleList from './components/ArticleList';
+import ArticleList from "./components/ArticleList";
 
 import ProgressBar from "./components/ProgressBar/ProgressBar";
+import List from "./components/List/List";
+
+import Table from "./components/Table/Table";
 
 function App() {
+  const example = [
+    ["Example",  " ", "Example",  " ", "Example"],
+    ["Example", " ", "Example",  " ", "Example"],
+    ["Example",  " ", "Example",  " ", "Example"],
+  ];
+
   const veggies = ["Carrot", "Potato", "Strawberry"];
   const fruits = [
-    {name: "Mango", color: "Yellow"},
-    {name: "Apple", color: "Green"},
-    {name: "Cherry", color: "Red"},
+    { name: "Mango", color: "Yellow" },
+    { name: "Apple", color: "Green" },
+    { name: "Cherry", color: "Red" },
   ];
-  const veggiesList = veggies.map((veggie) =>{
-    return<li>{veggie}</li>
+  const veggiesList = veggies.map((veggie) => {
+    return <li>{veggie}</li>;
   });
   const fruitsList = fruits.map((fruit) => (
     <li>
       {fruit.name}: {fruit.color}
     </li>
   ));
-  
 
   const articles = [
-    {title: "This is an example article", url: "#", teaser: "lorem"},
-    {title: "Another article as an example", url: "#", teaser: "lorem"},
-    {title: "Last example article", url: "#", teaser: "lorem"},
+    { title: "This is an example article", url: "#", teaser: "lorem" },
+    { title: "Another article as an example", url: "#", teaser: "lorem" },
+    { title: "Last example article", url: "#", teaser: "lorem" },
   ];
 
-  
-  
   return (
     <div className="App">
+      <h2>Homework</h2>
+       <Table src={example} />
       <h1>Hello world</h1>
-     <ul>
-     {veggiesList}
-     </ul>
-     <ol>{veggiesList}</ol>
-     <ul>
-     {fruitsList}
-     </ul>
+      <ul>{veggiesList}</ul>
+      <ol>{veggiesList}</ol>
+      <ul>{fruitsList}</ul>
+      <List src={veggies} />
 
-     <ArticleList src={articles}/>
- 
-     <ProgressBar progress={25} />
+      <ArticleList src={articles} />
+
+      <ProgressBar progress={25} />
       <ProgressBar progress={50} color="orange" />
       <ProgressBar progress={30} />
       <ProgressBar progress={60} />
       <ProgressBar progress={90} />
 
- 
+     
     </div>
   );
 }
